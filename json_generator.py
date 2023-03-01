681524356f6b5caf33138e8dafe9685f759beb9f
seankzw <56796695+seankzw@users.noreply.github.com>
2023-03-01 10:37:49 +0800

    Updated main.py

--- json_generator.py
+++ json_generator.py
@@ -2,7 +2,6 @@ import json
 import pandas as pd
 
 xl = pd.ExcelFile("./datas/bus_stops.xlsx")
-#print(xl.sheet_names)
 
 dict = {}
 
@@ -19,22 +18,12 @@ for eachSheetIdx in range(len(xl.sheet_names)):
         if row["Bus stop"] not in dict:
             dict[row["Bus stop"]] = {
                 "buses" : [sheetName],
-                #"coordinates": row["GPS Location"],
                 "coordinates": coordinatesVal
 
             }
         else:
             dict[row["Bus stop"]]["buses"] += [sheetName]
-    #if eachSheetData["Bus stop"] in dict:
-        #dict[eachSheetData["Bus stop"]]["buses"].append(sheetName)
-
-        #else:
-        #    bus_df.loc[i,"Bus stop"] = {
-        #        "buses" : [sheetName],
-        #        "coordinates": bus_df.loc[i,"GPS Location"],
-        #    }
 
 with open("data.json", "w") as outfile:
     json.dump(dict, outfile)
 
-#print(dict)
---
+++ main.py
@@ -0,0 +1,18 @@
+import json
+
+def dijkstra():
+    f = open("data.json")
+    data = json.load(f)
+    print(data)
+    pass
+
+def distanceCal(latx, lnbx, laty, lngy):
+    pass
+
+def distanceFromXtoB(latx, lngx, latb, lngb):
+    pass
+
+def distanceFromBusStop(latCurr, lngCurr, latb, lngb):
+    pass
+
+dijkstra()
