89aa5930d44af9f01e1ac230f32b1a6bf261a81c
veleonlim <62128001+veleonlim@users.noreply.github.com>
2023-03-12 20:22:35 +0800

    Update GUI.py
    Created/Plot Path btwn 2 points

--- GUI.py
+++ GUI.py
@@ -5,7 +5,7 @@ from geopy import distance
 from tkinter import messagebox
 import tkintermapview as tkmv
 
-
+#Create Window
 windows = tk.Tk()
 windows.geometry("800x600")
 windows.title("Maps")
@@ -15,43 +15,98 @@ geolocator = Nominatim(user_agent="myApp")
 # Create the left column for the input/label field
 left_frame = tk.Frame(windows)
 left_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
-left_frame.rowconfigure(1, weight=1)
+left_frame.rowconfigure(2, weight=1)
 
-# Create the label and input field
+# Create the label and input field for Start Location
 label = tk.Label(left_frame, text="Enter your start location:")
 label.grid(column=0, row=0, pady=10)
 
-# Create the input field
+# Create the input field for Start Location
 userInputLocation = tk.Entry(left_frame, width=20)
 userInputLocation.grid(column=1, row=0, padx=10, pady=10)
+    
+# Create the label and input field for End Location
+label2 = tk.Label(left_frame, text="Enter your end location:")
+label2.grid(column=0, row=1, pady=10)
 
+# Create the input field for End Location
+userInputLocation2 = tk.Entry(left_frame, width=20)
+userInputLocation2.grid(column=1, row=1, padx=10, pady=10)
 
+# # define global variables to store latitude and longitude
+# start_lat, start_long = None, None
+# end_lat, end_long = None, None
 
+def getStartLatLong():
+    # global start_lat, start_long
+    if userInputLocation.get() == '':
+        messagebox.showinfo("showinfo", "Enter Start Location")
 
-def getLatLong():
-    location = geolocator.geocode(userInputLocation.get() + " JB MY")
-    print(userInputLocation.get() + " JB MY")
-    label_lat = tk.Label(windows, text=location.latitude)
-    label_lat.pack()
-    label_long = tk.Label(windows, text=location.longitude)
-    label_long.pack()
-    messagebox.showinfo('Location', location.address)
-    print(str(location.latitude) + ", " + str(location.longitude))
+    else:
+        startLocation = geolocator.geocode(userInputLocation.get() + " JB MY")
+        print(userInputLocation.get() + " JB MY")
+        label_lat = tk.Label(windows, text=startLocation.latitude)
+        label_lat.pack()
+        label_long = tk.Label(windows, text=startLocation.longitude)
+        label_long.pack()
+        messagebox.showinfo('LWHwqeewqewqewqeqewewqewq', startLocation.address)
+        print(str(startLocation.latitude) + ", " + str(startLocation.longitude))
 
-    # create marker with custom colors and font
-    mapview.set_marker(location.latitude, location.longitude, text_color="red",
+        # create marker with custom colors and font
+        mapview.set_marker(startLocation.latitude, startLocation.longitude, text_color="red",
                                  marker_color_circle="white", marker_color_outside="red", font=("Helvetica Bold", 10))
+        
+    return (startLocation.latitude, startLocation.longitude)
+        # store latitude and longitude in global variables
+        # start_lat, start_long = startLocation.latitude, startLocation.longitude
+        
+
+def getEndLatLong():
+    # global end_lat, end_long
+    if userInputLocation2.get() == '':
+        messagebox.showinfo("showinfo", "Enter End Location")
+    
+    else:
+    
+        location = geolocator.geocode(userInputLocation2.get() + " JB MY")
+        print(userInputLocation2.get() + " JB MY")
+        label_lat = tk.Label(windows, text=location.latitude)
+        label_lat.pack()
+        label_long = tk.Label(windows, text=location.longitude)
+        label_long.pack()
+        messagebox.showinfo('Location', location.address)
+        print(str(location.latitude) + ", " + str(location.longitude))
+
+        # create marker with custom colors and font
+        mapview.set_marker(location.latitude, location.longitude, text_color="green",
+                                 marker_color_circle="white", marker_color_outside="green", font=("Helvetica Bold", 10))
+        
+    return (location.latitude, location.longitude)
+         # store latitude and longitude in global variables
+        # end_lat, end_long = location.latitude, location.longitude
+        
+def createPath():
+    location = getStartLatLong()
+    location2 = getEndLatLong()
+    if location and location2:
+        path = mapview.set_path([location, location2])
+        path.set_color("blue")
+        
+# def createPath():
+#     # call getStartLatLong() and getEndLatLong() to set global variables
+#     getStartLatLong()
+#     getEndLatLong()
+    
+#     # access global variables to set the path
+#     if start_lat and start_long and end_lat and end_long:
+#         path = mapview.set_path([(start_lat, start_long), (end_lat, end_long)])
+#         path.set_color("blue")
 
 
 
-
-
-
-# Create the "Enter" button
-button = tk.Button(left_frame, text="Enter", command=getLatLong)
-button.grid(column=1, row=1)
-
-
+# Create the "Create Path" button to create the path
+button3 = tk.Button(left_frame, text="Create Path", command=createPath)
+button3.grid(column=0, row=2)
 
 # Create the right column for the map
 right_frame = tk.Frame(windows)
@@ -66,6 +121,23 @@ mapview.set_zoom(12)
 mapview.grid(row=0, column=0, sticky="nsew")
 
 
+    # location = geolocator.geocode(userInputLocation.get() + " JB MY")
+    # location2 = geolocator.geocode(userInputLocation2.get() + " JB MY")
+ 
+    # print("Creating path...")
+    # start_lat = getStartLatLong.location.latitude
+    # start_lon = getStartLatLong.startLocation.longitude
+    # end_lat = getEndLatLong.location.latitude
+    # end_lon = getEndLatLong.location.longitude
+    
+    # # create the path
+    # path = mapview.set_path([(start_lat, start_lon), (end_lat, end_lon)])
+
+# set a path
+       
+# path_1.add_position(...)
+# path_1.remove_position(...)
+# path_1.delete()
 
 
 
