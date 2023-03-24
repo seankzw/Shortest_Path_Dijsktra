168eae4857152cb3673d7e0d6cabc1ac2f44b020
seankzw <56796695+seankzw@users.noreply.github.com>
2023-03-24 17:14:31 +0800

    update ui

--- GUI.py
+++ GUI.py
@@ -10,9 +10,7 @@ import tkintermapview as tkmv
 from Coordinates import Coordinates
 from brain import *
 
-# Create Window
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
+
 
 
 windows = ctk.CTk()
@@ -20,8 +18,11 @@ windows.geometry("800x600")
 windows.title("CSC1108 Johor Bahru Maps")
 windows.iconphoto(False, tk.PhotoImage(file="compass.png"))
 ctk.CTkFont("Helvetica")
-
 geolocator = Nominatim(user_agent="myApp")
+
+# Create Window
+ctk.set_default_color_theme("green")
+ctk.set_appearance_mode("dark")
 
 
 # Create the left column for the input/label field
@@ -29,8 +30,8 @@ left_frame = ctk.CTkFrame(windows)
 left_frame.pack(side="left", fill="both", expand=True, padx=10, pady=10)
 
 # Create the label and input field for Start Location
-label = ctk.CTkLabel(left_frame, justify="left", text="Enter your start location:")
-label.grid(column=0, row=0)
+label = ctk.CTkLabel(left_frame,justify="left", text="Enter your start location:")
+label.grid(column=0, row=0, padx=10, sticky="w")
 
 # Create the input field for Start Location
 userInputLocation = ctk.CTkEntry(left_frame, placeholder_text="Johor Zoo", width=250)
@@ -38,12 +39,20 @@ userInputLocation.grid(column=0, row=1, padx=10, pady=10)
 
 # Create the label and input field for End Location
 label2 = ctk.CTkLabel(left_frame, justify="left",text="Enter your end location:")
-label2.grid(column=0, row=2)
+label2.grid(column=0, row=2, padx=10, sticky="w")
 
 # Create the input field for End Location
 userInputLocation2 = ctk.CTkEntry(left_frame, placeholder_text="Paradigm Mall", width=250)
 userInputLocation2.grid(column=0, row=3, padx=10, pady=10)
 
+switch_var = ctk.StringVar(value="dark")
+def change_appearance_mode():
+    ctk.set_appearance_mode(switch_var.get())
+# Mode switcher ?
+#testMenu = ctk.CTkOptionMenu(left_frame, values=["Dark", "Light", "System"], command=change_appearance_mode)
+together=ctk.CTkFrame(windows)
+testMenu = ctk.CTkSwitch(left_frame, text="Dark Mode",command=change_appearance_mode, variable=switch_var, onvalue="dark",offvalue="light")
+testMenu.grid(row=5, column=0, sticky="w", padx=10, pady=10)
 # Create the label and input field for End Location
 
 # # define global variables to store latitude and longitude
@@ -55,6 +64,9 @@ def getStartLatLong():
     # global start_lat, start_long
     if userLoc == '':
         messagebox.showinfo("showinfo", "Please enter Start Location")
+    elif re.match("^-?[0-9].+$",userLoc):
+        print("User key in as coordinate")
+        return tuple(float(x) for x in userLoc.split(","))
     else:
         startLocation = geolocator.geocode(userLoc, country_codes="MY")
 
@@ -76,7 +88,9 @@ def getEndLatLong():
     # global end_lat, end_long
     if userLoc2 == '':
         messagebox.showinfo("showinfo", "Enter End Location")
-
+    elif re.match("^-?[0-9].+$",userLoc2):
+        print("User key in as coordinate")
+        return tuple(float(x) for x in userLoc2.split(","))
     else:
         endLocation = geolocator.geocode(userLoc2, country_codes="MY")
 
@@ -109,19 +123,19 @@ def createPath(left_frame):
     path_list = []
 
     # To display the paths
-    label = ctk.CTkLabel(left_frame, justify="left", text="Directions:")
-    label.grid(column=0, row=7)
+    label = ctk.CTkLabel(left_frame, width=250, justify="left", text="Directions:")
+    label.grid(column=0, row=8)
     routes = ctk.CTkTextbox(left_frame, width=250, height=350, scrollbar_button_color="white", )
-    routes.grid(column=0, row=8)
+    routes.grid(column=0, row=9)
     #routes.place(x=10, y=200)
     #routes.rowconfigure(5, weight=1)
     #routes.columnconfigure(1, weight=1)
 
-    #start_loc = Coordinates(1.5423777121603113, 103.62969894227055) #AEON
-    #end_loc = Coordinates(1.6349379250179437, 103.66630691168017) # Senai Airport Terminal
+    print("location = {}".format(location))
+    print("location2 = {}".format(location2))
+    print("location coord= {}".format(location[0]))
 
     start_bus_stop = findNearestStop(Coordinates(location[0], location[1]))
-    #end_bus_stop = findNearestStop(Coordinates(location2[0],location2[1]))
     end_bus_stops = findNearest5Stop(Coordinates(location2[0],location2[1]))
 
     distBetweenLoc = distanceBetween(Coordinates(location[0], location[1]), Coordinates(location2[0], location2[1]))
@@ -136,11 +150,14 @@ def createPath(left_frame):
     #Original code :
     #path_to_destination = getShortestPath(previous_node, shortest_path, start_bus_stop, end_bus_stop)
 
+    print("===== location[0], location[1] = {}, {}".format(location[0],location[1]))
+    print("===== location2[0], location2[1] = {}, {}".format(location2[0],location2[1]))
+
     path_to_destination, length = getShortestPathFromList(previous_node,start_bus_stop, end_bus_stops, Coordinates(location2[0],location2[1]))
     endBusStopCoordinate = getCoordFromBusStopName(path_to_destination[-1]["bus_stop_name"])
 
-    
-    if distBetweenLoc < distBetweenStartAndStop or distBetweenLoc > distanceBetween(endBusStopCoordinate, Coordinates(location2[0], location2[1])):
+    #if distBetweenLoc < distBetweenStartAndStop or distBetweenLoc > distanceBetween(endBusStopCoordinate, Coordinates(location2[0], location2[1])):
+    if distBetweenLoc > 2:
         boundingBox = getBoundingBox(location, location2)
         mapview.fit_bounding_box(boundingBox[0],boundingBox[1])
         #mapview.fit_bounding_box(location2, location)
@@ -168,9 +185,9 @@ def createPath(left_frame):
             # create marker with custom colors and font for this stop
             polygon_name = eachStop["bus_stop_name"] + "\n" + res
             print(path_to_destination[-1])
-            
+
             mapview.set_polygon([(eachStop["coordinates"][0], eachStop["coordinates"][1]), (eachStop["coordinates"][0], eachStop["coordinates"][1])], outline_color="red", border_width=12, command=polygonClicked, name=polygon_name)
-            
+
             mapview.set_marker(path_to_destination[0]["coordinates"][0], path_to_destination[0]["coordinates"][1], "Walk to " + path_to_destination[0]["bus_stop_name"], marker_color_circle="white", marker_color_outside="blue" )
             mapview.set_marker(path_to_destination[-1]["coordinates"][0], path_to_destination[-1]["coordinates"][1], "Walk to " + userInputLocation2.get(), marker_color_circle="white", marker_color_outside="blue")
 
@@ -180,7 +197,7 @@ def createPath(left_frame):
         # WALK TO DESTINATION
         endstop = geolocator.geocode(location2, country_codes="MY")
         routes.insert(END, "Walk {:.2f}km to {} \n\n".format(distBetweenStartAndStop, location2 if endstop == None else endstop))
-    
+
     routes.configure(state=tk.DISABLED)
 
 
@@ -195,11 +212,10 @@ def add_end_loc(coord):
     return
 
 
-
 # Create the "Create Path" button to create the path
-action_with_arg = partial(createPath, left_frame)
+action_with_arg= partial(createPath, left_frame)
 button3 = ctk.CTkButton(left_frame, text="Create Path", command=action_with_arg)
-button3.grid(column=0, row=5)
+button3.grid(column=0, row=5, columnspan=10,sticky="e")
 
 # Create the right column for the map
 right_frame = tk.Frame(windows)
@@ -207,33 +223,13 @@ right_frame.pack(side="left", fill="both", expand=True)
 right_frame.columnconfigure(0, weight=1)
 right_frame.rowconfigure(0, weight=1)
 
-# Create mapview
+# Create mapview with right click options
 mapview = tkmv.TkinterMapView(right_frame, width=800, height=600, corner_radius=0)
 mapview.add_right_click_menu_command(label="Add start location", command=add_start_loc, pass_coords=True)
 mapview.add_right_click_menu_command(label="Add end location", command=add_end_loc, pass_coords=True)
 mapview.set_address("JB, MY")
 mapview.set_zoom(12)
 mapview.grid(row=0, column=0, sticky="nsew")
-
-
-    # location = geolocator.geocode(userInputLocation.get() + " JB MY")
-    # location2 = geolocator.geocode(userInputLocation2.get() + " JB MY")
-
-    # print("Creating path...")
-    # start_lat = getStartLatLong.location.latitude
-    # start_lon = getStartLatLong.startLocation.longitude
-    # end_lat = getEndLatLong.location.latitude
-    # end_lon = getEndLatLong.location.longitude
-
-    # # create the path
-    # path = mapview.set_path([(start_lat, start_lon), (end_lat, end_lon)])
-
-# set a path
-
-# path_1.add_position(...)
-# path_1.remove_position(...)
-# path_1.delete()
-
 
 
 # prevent the resize of window
